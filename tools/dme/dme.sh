c2a8f10e608a27f8367ad707bf2c56fa2e541cfb
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-04 15:58:02 +0000

    feat: add more distros and alphabetically order all entries

--- tools/dme/dme.sh
+++ tools/dme/dme.sh
@@ -7,23 +7,28 @@ usage() {
     echo "Drop into a shell of specified Linux distribution"
     echo
     echo "Available distributions:"
+    echo "  alma        - Latest AlmaLinux"
     echo "  alpine      - Latest Alpine"
     echo "  alpine3.18  - Alpine 3.18"
     echo "  alpine3.17  - Alpine 3.17"
     echo "  amazonlinux - Amazon Linux 2"
     echo "  arch        - Latest Arch Linux"
     echo "  centos7     - CentOS 7"
+    echo "  clear       - Intel's Clear Linux"
     echo "  debian      - Latest Debian"
     echo "  debian11    - Debian 11"
     echo "  debian10    - Debian 10"
     echo "  fedora      - Latest Fedora"
+    echo "  gentoo      - Latest Gentoo Linux"
     echo "  kali        - Latest Kali Linux"
     echo "  nixos       - Latest NixOS"
+    echo "  openmandriva - Latest OpenMandriva"
     echo "  opensuse    - Latest openSUSE Leap"
     echo "  oracle      - Latest Oracle Linux"
     echo "  rocky       - Latest Rocky Linux"
     echo "  rocky9      - Rocky Linux 9"
     echo "  rocky8      - Rocky Linux 8"
+    echo "  slackware   - Latest Slackware"
     echo "  tumbleweed  - openSUSE Tumbleweed"
     echo "  ubuntu      - Latest Ubuntu"
     echo "  ubuntu22    - Ubuntu 22.04"
@@ -46,36 +51,8 @@ fi
 
 # Map distro names to docker images
 case "$1" in
-    "ubuntu"|"ubuntu22")
-        image="ubuntu:22.04"
-        shell="/bin/bash"
-        ;;
-    "ubuntu20")
-        image="ubuntu:20.04"
-        shell="/bin/bash"
-        ;;
-    "debian"|"debian11")
-        image="debian:11"
-        shell="/bin/bash"
-        ;;
-    "debian10")
-        image="debian:10"
-        shell="/bin/bash"
-        ;;
-    "centos7")
-        image="centos:7"
-        shell="/bin/bash"
-        ;;
-    "rocky"|"rocky9")
-        image="rockylinux:9"
-        shell="/bin/bash"
-        ;;
-    "rocky8")
-        image="rockylinux:8"
-        shell="/bin/bash"
-        ;;
-    "fedora")
-        image="fedora:latest"
+    "alma")
+        image="almalinux:latest"
         shell="/bin/bash"
         ;;
     "alpine"|"alpine3.18")
@@ -86,32 +63,80 @@ case "$1" in
         image="alpine:3.17"
         shell="/bin/sh"
         ;;
+    "amazonlinux")
+        image="amazonlinux:2"
+        shell="/bin/bash"
+        ;;
     "arch")
         image="archlinux:latest"
         shell="/bin/bash"
         ;;
-    "nixos")
-        image="nixos/nix:latest"
+    "centos7")
+        image="centos:7"
         shell="/bin/bash"
         ;;
-    "opensuse")
-        image="opensuse/leap:latest"
+    "clear")
+        image="clearlinux:latest"
         shell="/bin/bash"
         ;;
-    "tumbleweed")
-        image="opensuse/tumbleweed:latest"
+    "debian"|"debian11")
+        image="debian:11"
+        shell="/bin/bash"
+        ;;
+    "debian10")
+        image="debian:10"
+        shell="/bin/bash"
+        ;;
+    "fedora")
+        image="fedora:latest"
+        shell="/bin/bash"
+        ;;
+    "gentoo")
+        image="gentoo/stage3:latest"
         shell="/bin/bash"
         ;;
     "kali")
         image="kalilinux/kali-rolling:latest"
         shell="/bin/bash"
         ;;
-    "amazonlinux")
-        image="amazonlinux:2"
+    "nixos")
+        image="nixos/nix:latest"
+        shell="/bin/bash"
+        ;;
+    "openmandriva")
+        image="openmandriva/cooker:latest"
+        shell="/bin/bash"
+        ;;
+    "opensuse")
+        image="opensuse/leap:latest"
         shell="/bin/bash"
         ;;
     "oracle")
         image="oraclelinux:9"
+        shell="/bin/bash"
+        ;;
+    "rocky"|"rocky9")
+        image="rockylinux:9"
+        shell="/bin/bash"
+        ;;
+    "rocky8")
+        image="rockylinux:8"
+        shell="/bin/bash"
+        ;;
+    "slackware")
+        image="vbatts/slackware:latest"
+        shell="/bin/bash"
+        ;;
+    "tumbleweed")
+        image="opensuse/tumbleweed:latest"
+        shell="/bin/bash"
+        ;;
+    "ubuntu"|"ubuntu22")
+        image="ubuntu:22.04"
+        shell="/bin/bash"
+        ;;
+    "ubuntu20")
+        image="ubuntu:20.04"
         shell="/bin/bash"
         ;;
     "void")
