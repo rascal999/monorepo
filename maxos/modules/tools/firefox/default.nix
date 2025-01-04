a24a9ade532a35a234a944efd67a9b1577fbe3de
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-04 12:07:46 +0000

    Release mcq/v1.1.1

--- maxos/modules/tools/firefox/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  # Firefox system-wide configuration
-  environment.systemPackages = [ pkgs.firefox ];
-
-  # System-wide policies
-  environment.etc."firefox/policies/policies.json".source = ./policies.json;
-}
