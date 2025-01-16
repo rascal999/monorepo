000e230f82f1a5540d520f517f48f4ab6fab7b32
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-16 15:10:58 +0000

    chore: remove old project structure

--- tools/postman_to_pytest/scripts/update_class_name.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Update all test files to use ContentGenerator instead of TestContentGenerator
-find tests/generator -type f -name "test_*.py" -exec sed -i 's/TestContentGenerator/ContentGenerator/g' {} +
