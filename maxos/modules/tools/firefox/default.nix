0394a45077442a513c3d5e42a0d67475b1f252a9
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 19:40:55 +0000

    refactor(firefox): use home-manager policies for extension management

--- maxos/modules/tools/firefox/default.nix
+++ maxos/modules/tools/firefox/default.nix
@@ -1,18 +1,33 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.firefox.profiles.default = {
-    settings = {
-      # Enable dark theme
-      "browser.theme.content-theme" = 0;
-      "browser.theme.toolbar-theme" = 0;
-      # Dark mode preferences
-      "browser.in-content.dark-mode" = true;
-      "ui.systemUsesDarkTheme" = 1;
+  programs.firefox = {
+    enable = true;
+    policies = {
+      DisableFirefoxAccounts = true;
+      DisableProfileImport = true;
+      NoDefaultBookmarks = true;
+      DisplayMenuBar = "default-off";
+      ExtensionSettings = {
+        "uBlock0@raymondhill.net" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+        };
+        "addon@darkreader.org" = {
+          installation_mode = "force_installed";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+        };
+      };
     };
-    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
-      darkreader
-      ublock-origin
-    ];
+    profiles.default = {
+      settings = {
+        # Enable dark theme
+        "browser.theme.content-theme" = 0;
+        "browser.theme.toolbar-theme" = 0;
+        # Dark mode preferences
+        "browser.in-content.dark-mode" = true;
+        "ui.systemUsesDarkTheme" = 1;
+      };
+    };
   };
 }
