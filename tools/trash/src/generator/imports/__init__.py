471d214d72ff8a231faa0e92750e76a8817fac28
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 09:48:00 +0000

    chore: Remove tools/trash directory

--- tools/trash/src/generator/imports/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Import generation utilities."""
-
-from .imports_generator import ImportsGenerator
-
-__all__ = [
-    'ImportsGenerator',
-]
--- tools/trash/src/parser/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Parser module for handling Postman collection JSON and dependency YAML files.
-"""
-
-from .collection import PostmanCollectionParser
-from .dependency import DependencyGraphParser
-
-__all__ = ["PostmanCollectionParser", "DependencyGraphParser"]
