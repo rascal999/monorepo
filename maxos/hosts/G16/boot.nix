3b7a5085dfef7838645d679a7fdecf8613db1453
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 22:52:48 +0000

    Revert "fix(G16): use vendor backlight control based on G16 2024 guide"
    This reverts commit 40f1550b50e16a3542ff7398326a64e8c1ccedbb.

--- maxos/hosts/G16/boot.nix
+++ maxos/hosts/G16/boot.nix
@@ -11,9 +11,8 @@
       "acpi_osi=Linux" 
       "acpi_osi=!Windows2013" 
       "acpi_osi=!Windows2012"
-      "acpi_backlight=vendor"
-      "module.sig_enforce=0"
-      "nvidia-drm.modeset=1"
+      "acpi_backlight=native"
+      "asus.use_native_led=1"
     ];
     initrd = {
       availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod" "rtsx_pci_sdmmc" ];
