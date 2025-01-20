8552f8c1ce35d09423130dd70c1b4a378013d978
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 19:07:58 +0000

    fix(G16): update OpenGL config with VAAPI/VDPAU support

--- maxos/hosts/G16/default.nix
+++ maxos/hosts/G16/default.nix
@@ -104,8 +104,14 @@
     };
     opengl = {
       enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
+      extraPackages = with pkgs; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+      extraPackages32 = with pkgs.pkgsi686Linux; [
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
     };
   };
 
