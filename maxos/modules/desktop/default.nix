a9a262a482ec2844b3835c1e6c63d2a255d91669
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 18:45:51 +0000

    refactor(dpi): move DPI settings to .Xresources

--- maxos/modules/desktop/default.nix
+++ maxos/modules/desktop/default.nix
@@ -9,17 +9,11 @@
     ];
   };
 
-  # HiDPI settings
+  # X server configuration
   services.xserver = {
-    dpi = 192; # Adjust based on screen
     displayManager = {
       sessionCommands = ''
-        # Set GDK scaling
-        export GDK_SCALE=2
-        export GDK_DPI_SCALE=0.5
-        # Set Qt scaling
-        export QT_AUTO_SCREEN_SCALE_FACTOR=1
-        # Set Firefox scaling
+        # Enable Firefox touchpad gestures
         export MOZ_USE_XINPUT2=1
       '';
     };
