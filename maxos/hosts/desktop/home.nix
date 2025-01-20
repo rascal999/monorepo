dd363609de202eb5cc761021d0bee8534b5417b9
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 19:28:00 +0000

    feat(firefox): add dark theme and essential extensions

--- maxos/hosts/desktop/home.nix
+++ maxos/hosts/desktop/home.nix
@@ -47,7 +47,6 @@
       libreoffice
       vlc
       gimp
-      firefox
       
       # Communication
       slack
@@ -96,6 +95,26 @@
 
   # Additional program configurations
   programs = {
+    firefox = {
+      enable = true;
+      profiles.default = {
+        id = 0;
+        name = "Default";
+        settings = {
+          # Enable dark theme
+          "browser.theme.content-theme" = 0;
+          "browser.theme.toolbar-theme" = 0;
+          # Dark mode preferences
+          "browser.in-content.dark-mode" = true;
+          "ui.systemUsesDarkTheme" = 1;
+        };
+        extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+          darkreader
+          ublock-origin
+        ];
+      };
+    };
+
     bash = {
       enable = true;
       shellAliases = {
