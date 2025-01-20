27203741ffe8baf2ba05b152792e9d918bce80fa
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 18:40:19 +0000

    fix(i3): remove redundant workspace assignments from desktop config

--- maxos/modules/tools/i3/desktop.nix
+++ maxos/modules/tools/i3/desktop.nix
@@ -12,12 +12,6 @@
       { command = "firefox"; notification = false; }
     ];
 
-    # Additional workspace assignments for desktop applications
-    assigns = {
-      "1" = [{ class = "^Firefox$"; }];
-      "2" = [{ class = "^Code$"; }];
-      "5: media" = [{ class = "^Spotify$"; }];
-    };
   };
 
   # Additional packages for desktop environment
