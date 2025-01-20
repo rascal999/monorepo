33c9a3c5150a46b4fabbb158dd98626e16e3ef2e
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 21:52:45 +0000

    fix(desktop): remove sct configuration
    - Remove sct systemd service
    - Remove sct from system packages
    - Remove video group configuration for sct

--- maxos/modules/desktop/default.nix
+++ maxos/modules/desktop/default.nix
@@ -9,25 +9,6 @@
     ];
   };
 
-  # Add user to video group for screen color temperature control
-  users.users.user.extraGroups = [ "video" ];
-
-  # Configure sct for constant warm color temperature
-  systemd.services.sct = {
-    description = "Set color temperature using sct";
-    wantedBy = [ "multi-user.target" ];
-    after = [ "display-manager.service" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-      User = "user";
-      Group = "video";
-      ExecStart = "${pkgs.sct}/bin/sct 1900";
-      ExecStop = "${pkgs.sct}/bin/sct";
-      Environment = "DISPLAY=:0 XAUTHORITY=/home/user/.Xauthority";
-    };
-  };
-
   # X server configuration
   services.xserver = {
     enable = true;
@@ -38,7 +19,6 @@
   };
 
   environment.systemPackages = with pkgs; [
-    sct
     xorg.xrandr
     pciutils  # Provides lspci command
   ];
