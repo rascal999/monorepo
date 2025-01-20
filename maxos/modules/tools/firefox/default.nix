b27f35b21808f883f90078a0b1471e9660b4b2ad
Aidan Marlin <aidan.marlin at gmail.com>
2025-01-20 19:30:33 +0000

    refactor(firefox): move configuration to module

--- maxos/modules/tools/firefox/default.nix
+++ maxos/modules/tools/firefox/default.nix
@@ -1,6 +1,30 @@
 { config, lib, pkgs, ... }:
 
 {
-  # Firefox is now managed by home-manager
-  # See ../../../hosts/desktop/home.nix for configuration
+  programs.firefox = {
+    enable = true;
+    profiles.default = {
+      id = 0;
+      name = "Default";
+      settings = {
+        # Enable dark theme
+        "browser.theme.content-theme" = 0;
+        "browser.theme.toolbar-theme" = 0;
+        # Dark mode preferences
+        "browser.in-content.dark-mode" = true;
+        "ui.systemUsesDarkTheme" = 1;
+      };
+      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        darkreader
+        ublock-origin
+      ];
+    };
+  };
+
+  # Remove Firefox from system packages since it's managed by home-manager
+  home.packages = lib.mkAfter (with pkgs; [
+    # Remove Firefox if it exists in the list
+    # This is done using a filter function
+    (lib.lists.filter (p: p.name != "firefox") config.home.packages)
+  ]);
 }
