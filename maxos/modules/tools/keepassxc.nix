3135cac77347de41052ace79bb0e410a1142f13e
Your Name <your.email@example.com>
2025-01-21 17:25:39 +0000

    feat(keepassxc): add native messaging support for Firefox integration

--- maxos/modules/tools/keepassxc.nix
+++ maxos/modules/tools/keepassxc.nix
@@ -43,5 +43,18 @@ ExcludedChars=
       '';
       force = true;
     };
+
+    # Enable native messaging for browser integration
+    home.file.".mozilla/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".text = ''
+{
+    "allowed_extensions": [
+        "keepassxc-browser@keepassxc.org"
+    ],
+    "description": "KeePassXC integration with native messaging support",
+    "name": "org.keepassxc.keepassxc_browser",
+    "path": "${pkgs.keepassxc}/bin/keepassxc-proxy",
+    "type": "stdio"
+}
+    '';
   };
 }
