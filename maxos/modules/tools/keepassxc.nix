f7be76c62e0258a1bd8e1db7ff7296e1293f5e65
Your Name <your.email@example.com>
2025-01-21 17:36:35 +0000

    fix(keepassxc): use system activation script for config

--- maxos/modules/tools/keepassxc.nix
+++ maxos/modules/tools/keepassxc.nix
@@ -6,17 +6,23 @@
     keepassxc
   ];
 
-  # Configure dark theme
-  home-manager.users.user = {
-    xdg.configFile."keepassxc/keepassxc.ini" = {
-      text = ''
-[General]
+  # Create KeePassXC config with dark theme
+  system.activationScripts.keepassxcConfig = {
+    text = ''
+      CONFIG_DIR="/home/user/.config/keepassxc"
+      CONFIG_FILE="$CONFIG_DIR/keepassxc.ini"
+      
+      mkdir -p "$CONFIG_DIR"
+      
+      echo '[General]
 ConfigVersion=2
 
 [GUI]
-ApplicationTheme=dark
-      '';
-      force = true;
-    };
+ApplicationTheme=dark' > "$CONFIG_FILE"
+      
+      chown -R user:users "$CONFIG_DIR"
+      chmod 644 "$CONFIG_FILE"
+    '';
+    deps = [];
   };
 }
