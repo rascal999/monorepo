0444050c697d7747f581a66620034587ade373a6
Your Name <your.email@example.com>
2025-01-21 17:37:32 +0000

    fix(keepassxc): use home-manager home.file for config

--- maxos/modules/tools/keepassxc.nix
+++ maxos/modules/tools/keepassxc.nix
@@ -6,23 +6,14 @@
     keepassxc
   ];
 
-  # Create KeePassXC config with dark theme
-  system.activationScripts.keepassxcConfig = {
-    text = ''
-      CONFIG_DIR="/home/user/.config/keepassxc"
-      CONFIG_FILE="$CONFIG_DIR/keepassxc.ini"
-      
-      mkdir -p "$CONFIG_DIR"
-      
-      echo '[General]
+  # Configure dark theme using home-manager
+  home-manager.users.user.home.file = {
+    ".config/keepassxc/keepassxc.ini".text = ''
+[General]
 ConfigVersion=2
 
 [GUI]
-ApplicationTheme=dark' > "$CONFIG_FILE"
-      
-      chown -R user:users "$CONFIG_DIR"
-      chmod 644 "$CONFIG_FILE"
+ApplicationTheme=dark
     '';
-    deps = [];
   };
 }
