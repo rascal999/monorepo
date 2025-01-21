88325dc7dda9649480181b12e8a9eb8bdb575b11
Your Name <your.email@example.com>
2025-01-21 19:04:47 +0000

    Revert "fix(G16): simplify i3status-rust config and fix redshift script installation"
    This reverts commit 199fdd1548167973f0608d50e15f4a98c0111e18.

--- maxos/modules/tools/i3/status.nix
+++ maxos/modules/tools/i3/status.nix
@@ -1,82 +1,89 @@
 { config, lib, pkgs, ... }:
 
-let
-  redshiftScript = pkgs.writeShellScriptBin "redshift-brightness" (builtins.readFile ../../desktop/redshift-brightness.sh);
-in
 {
   home.packages = with pkgs; [
     i3status-rust
-    redshiftScript
   ];
 
   # Configure i3status-rust
-  xdg.configFile."i3status-rust/config.toml".text = ''
-    theme = "nord-dark"
-    icons = "material-nf"
-
-    [[block]]
-    block = "custom"
-    command = "redshift-brightness get"
-    on_click = ""
-    on_click_right = ""
-    on_scroll_up = "redshift-brightness up"
-    on_scroll_down = "redshift-brightness down"
-    interval = 1
-    format = " 󰃟 $text "
-
-    [[block]]
-    block = "sound"
-    format = " $icon {$volume|} "
-
-    [[block]]
-    block = "net"
-    format = " $icon {$signal_strength $ssid|Wired} "
-    format_alt = " $icon {$signal_strength $ssid|Wired} $ip "
-
-    [[block]]
-    block = "disk_space"
-    path = "/"
-    info_type = "available"
-    alert_unit = "GB"
-    interval = 20
-    warning = 20.0
-    alert = 10.0
-    format = " $icon $available "
-
-    [[block]]
-    block = "memory"
-    format = " $icon $mem_used_percents "
-    format_alt = " $icon_swap $swap_used_percents "
-
-    [[block]]
-    block = "load"
-    interval = 1
-    format = " $icon $1m.eng(w:4) $15m.eng(w:4) "
-
-    [[block]]
-    block = "cpu"
-    interval = 1
-    format = " $icon $frequency.eng(w:4) $utilization.eng(w:3) "
-
-    [[block]]
-    block = "battery"
-    format = " $icon $percentage {$time |}"
-    device = "BAT1"
-    missing_format = ""
-
-    [[block]]
-    block = "time"
-    interval = 5
-    format = " $icon $timestamp.datetime(f:'%V %m/%d %R') "
-
-    [theme.overrides]
-    separator = ""
-    separator_fg = "auto"
-    separator_bg = "auto"
-    idle_bg = "#000000"
-    info_bg = "#2e3f54"
-    good_bg = "#000000"
-    warning_bg = "#755f2d"
-    critical_bg = "#542a2e"
-  '';
+  programs.i3status-rust = {
+    enable = true;
+    bars = {
+      default = {
+        theme = "nord-dark";
+        icons = "material-nf";
+        settings = {
+          theme = {
+            overrides = {
+              separator = "";
+              separator_fg = "auto";
+              separator_bg = "auto";
+              idle_bg = "#000000";
+              info_bg = "#2e3f54";
+              good_bg = "#000000";
+              warning_bg = "#755f2d";
+              critical_bg = "#542a2e";
+            };
+          };
+        };
+        blocks = [
+          {
+            block = "custom";
+            command = "${pkgs.writeShellScriptBin "redshift-brightness" (builtins.readFile ../../desktop/redshift-brightness.sh)}/bin/redshift-brightness get";
+            on_click = "";
+            on_click_right = "";
+            on_scroll_up = "${pkgs.writeShellScriptBin "redshift-brightness" (builtins.readFile ../../desktop/redshift-brightness.sh)}/bin/redshift-brightness up";
+            on_scroll_down = "${pkgs.writeShellScriptBin "redshift-brightness" (builtins.readFile ../../desktop/redshift-brightness.sh)}/bin/redshift-brightness down";
+            interval = 1;
+            format = " 󰃟 $text ";
+          }
+          {
+            block = "sound";
+            format = " $icon {$volume|} ";
+          }
+          {
+            block = "net";
+            format = " $icon {$signal_strength $ssid|Wired} ";
+            format_alt = " $icon {$signal_strength $ssid|Wired} $ip ";
+          }
+          {
+            block = "disk_space";
+            path = "/";
+            info_type = "available";
+            alert_unit = "GB";
+            interval = 20;
+            warning = 20.0;
+            alert = 10.0;
+            format = " $icon $available ";
+          }
+          {
+            block = "memory";
+            format = " $icon $mem_used_percents ";
+            format_alt = " $icon_swap $swap_used_percents ";
+          }
+          {
+            block = "load";
+            interval = 1;
+            format = " $icon $1m.eng(w:4) $15m.eng(w:4) ";
+          }
+          {
+            block = "cpu";
+            interval = 1;
+            format = " $icon $frequency.eng(w:4) $utilization.eng(w:3) ";
+          }
+          {
+            block = "battery";
+            format = " $icon $percentage {$time |}";
+            device = "BAT1";
+            missing_format = "";
+          }
+          {
+            block = "time";
+            interval = 5;
+            format = " $icon $timestamp.datetime(f:'%V %m/%d %R') ";
+          }
+        ];
+      };
+    };
+  };
 }
