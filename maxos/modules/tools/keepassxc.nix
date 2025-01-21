88a452551d721042200fe7ad8139a47df8c4a6b6
Your Name <your.email@example.com>
2025-01-21 17:38:59 +0000

    feat(desktop): add global GTK dark theme, simplify keepassxc

--- maxos/modules/tools/keepassxc.nix
+++ maxos/modules/tools/keepassxc.nix
@@ -5,15 +5,4 @@
   environment.systemPackages = with pkgs; [
     keepassxc
   ];
-
-  # Configure dark theme using home-manager
-  home-manager.users.user.home.file = {
-    ".config/keepassxc/keepassxc.ini".text = ''
-[General]
-ConfigVersion=2
-
-[GUI]
-ApplicationTheme=dark
-    '';
-  };
 }
