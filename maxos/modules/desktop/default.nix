7ad0b556181399d57c19a4e61e7df9b5c087bce9
Your Name <your.email@example.com>
2025-01-21 01:18:50 +0000

    feat(desktop): add system-wide GTK dark theme configuration

--- maxos/modules/desktop/default.nix
+++ maxos/modules/desktop/default.nix
@@ -22,5 +22,47 @@
     xorg.xrandr
     pciutils  # Provides lspci command
     bc  # For floating point calculations in brightness control
+    gnome.adwaita-icon-theme
   ];
+
+  # GTK configuration
+  qt.enable = true;
+  qt.platformTheme = "gtk2";
+  qt.style = "adwaita-dark";
+
+  # GTK theme settings
+  environment.sessionVariables = {
+    GTK_THEME = "Adwaita:dark";
+  };
+
+  # System-wide GTK settings
+  programs.dconf.enable = true;
+  
+  # GTK configuration
+  services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    epiphany    # Web browser
+    totem       # Video player
+    geary       # Email client
+    seahorse    # Password manager
+  ];
+
+  # Force GTK applications to use dark theme
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Adwaita-dark";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = true;
+    };
+  };
 }
