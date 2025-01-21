252cec8e55cb66bbb477622f1e876073c2daed1f
Your Name <your.email@example.com>
2025-01-21 19:50:02 +0000

    refactor(G16): simplify i3status-rust config and use backlight block

--- maxos/modules/tools/i3/status.nix
+++ maxos/modules/tools/i3/status.nix
@@ -1,12 +1,8 @@
 { config, lib, pkgs, ... }:
 
-let
-  redshiftScript = pkgs.writeShellScriptBin "redshift-brightness" (builtins.readFile ../../desktop/redshift-brightness.sh);
-in
 {
   home.packages = with pkgs; [
     i3status-rust
-    redshiftScript
   ];
 
   # Configure i3status-rust
