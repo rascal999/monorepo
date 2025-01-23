2b8d339702ccbaefcd327937f0b326800567181b
Your Name <your.email@example.com>
2025-01-23 22:20:38 +0000

    fix: update scripts module for mvpn

--- maxos/modules/scripts/default.nix
+++ maxos/modules/scripts/default.nix
@@ -4,6 +4,7 @@ let
   maxosScript = pkgs.writeShellScriptBin "maxos" (builtins.readFile ../../scripts/maxos);
   redshiftBrightness = pkgs.writeShellScriptBin "redshift-brightness" (builtins.readFile ../../scripts/redshift-brightness);
   clearUrgent = pkgs.writeShellScriptBin "clear-urgent" (builtins.readFile ../../scripts/clear-urgent);
+  mvpnScript = pkgs.writeShellScriptBin "mvpn" (builtins.readFile ../../scripts/mvpn);
 in
 {
   config = {
@@ -11,6 +12,7 @@ in
       maxosScript
       redshiftBrightness
       clearUrgent
+      mvpnScript
       # Dependencies for redshift-brightness
       pkgs.bc  # For floating point calculations
       pkgs.jq  # For JSON output in get command
