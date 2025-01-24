6e9c13195b5c15803fa9706430cc12c9b47ad32d
Your Name <your.email@example.com>
2025-01-24 17:27:20 +0000

    awscli

--- maxos/modules/desktop/default.nix
+++ maxos/modules/desktop/default.nix
@@ -24,6 +24,7 @@
 
   environment.systemPackages = with pkgs; [
     adwaita-icon-theme
+    awscli
     bc  # For floating point calculations in brightness control
     maim
     pciutils  # Provides lspci command
