92721698d96937e14af210a1b1e04bc311d96967
Your Name <your.email@example.com>
2025-01-30 00:06:51 +0000

    goose gitlab

--- tools/goose/docker/run-goose.sh
+++ tools/goose/docker/run-goose.sh
@@ -33,6 +33,7 @@ fi
 # Run bash which launches goose, keeping goose in command history
 docker run --rm -it \
   -p 5173:5173 \
+  -p 3000:3000 \
   $MCP_MOUNTS \
   -v "${WORKSPACE_DIR}:/workspace" \
   -v "${MEMORY_DIR}:/root/.config/goose/memory" \
@@ -44,5 +45,9 @@ docker run --rm -it \
   -e JIRA_URL="${JIRA_URL}" \
   -e JIRA_USERNAME="${JIRA_USERNAME}" \
   -e JIRA_API_TOKEN="${JIRA_API_TOKEN}" \
+  -e GITLAB_PERSONAL_ACCESS_TOKEN="${GITLAB_PERSONAL_ACCESS_TOKEN}" \
+  -e GITLAB_API_URL="${GITLAB_API_URL}" \
+  -e SLACK_BOT_TOKEN="${SLACK_BOT_TOKEN}" \
+  -e SLACK_TEAM_ID="${SLACK_TEAM_ID}" \
   --entrypoint bash \
   goose -c 'cmd="goose $*"; echo "$cmd" >> ~/.bash_history; HISTFILE=~/.bash_history; history -r; eval "$cmd"; exec bash --init-file <(echo "history -r")' _ "$@"
