6cb7cab65b8e7dee2050c51dc1f92a3ab740373c
Your Name <your.email@example.com>
2025-02-26 16:58:35 +0000

    maxos rig DP-4 144Hz

--- maxos/hosts/rig/monitors.nix
+++ maxos/hosts/rig/monitors.nix
@@ -3,6 +3,6 @@
 {
   # Add xrandr command to X server startup
   services.xserver.displayManager.setupCommands = ''
-    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --primary --mode 3440x1440 --rate 144 --output DP-4 --mode 2560x1440 --rate 120 --left-of DP-2
+    ${pkgs.xorg.xrandr}/bin/xrandr --output DP-2 --primary --mode 3440x1440 --rate 144 --output DP-4 --mode 2560x1440 --rate 144 --left-of DP-2
   '';
 }
